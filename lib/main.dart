7306f4b037ee1d3b3382233175b99db239cf296f
Yuri V. Huallpa Vargas <yurihuallpavargas@gmail.com>
2021-12-18 12:36:28 -0500

    sign in and sign out with google, android and ios

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FloatingActionButton.extended(
+          onPressed: () {},
+          label: const Text("Sign in with Google"),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          icon: Image.asset("/assets/google.png", width: 32, height: 32),
+        ),
+      ),
+    );
+  }
+}
